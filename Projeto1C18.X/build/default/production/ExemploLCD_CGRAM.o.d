4e3a7e9c434ffa891b0e911830aac639e608c325
SanUSB <sanusb_laese@yahoo.com.br>
2015-05-24 16:22:52 -0300

    Smart1
    S1

---
+++ Projeto1C18.X/build/default/production/ExemploLCD_CGRAM.o.d
@@ -0,0 +1,5 @@
+build/default/production/ExemploLCD_CGRAM.o:  \
+	C:/Users/Sandro/Dropbox/GitHub/LCD-16x2-Smart-CGRAM-PIC/Projeto1C18.X/ExemploLCD_CGRAM.c  \
+	C:/Users/Sandro/Dropbox/GitHub/LCD-16x2-Smart-CGRAM-PIC/Projeto1C18.X/SanUSB48.h  \
+	C:/Users/Sandro/Dropbox/GitHub/LCD-16x2-Smart-CGRAM-PIC/Projeto1C18.X/lcd.h  \
+
---
+++ Projeto1XC8.X/build/default/production/ExemploLCD_CGRAM.p1.d
@@ -0,0 +1,5 @@
+ build/default/production/ExemploLCD_CGRAM.d  \
+ build/default/production/ExemploLCD_CGRAM.p1:  \
+ ExemploLCD_CGRAM.c  \
+lcd.h  \
+SanUSB48X.h 
